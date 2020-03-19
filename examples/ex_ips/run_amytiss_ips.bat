321f48455c649275ac885764902cf808cfda88d5
M. Khaled (MBP) <eng.mk@msn.com>
2020-03-19 10:21:44 +0100

    Automated commit from the Makeflle

--- examples/ex_ips/run_amytiss_ips.bat
+++ examples/ex_ips/run_amytiss_ips.bat
@@ -1,3 +1,2 @@
-pfaces -CG -k amytiss.cpu@..\..\kernel-pack -cfg ips2d.cfg -p -d 1
 pfaces -CG -k amytiss.cpu@..\..\kernel-pack -cfg ips4d.cfg -p -d 1
 pause
--- examples/ex_ips/run_amytiss_ips.sh
+++ examples/ex_ips/run_amytiss_ips.sh
@@ -1,4 +1,3 @@
-pfaces -CG -k amytiss.cpu@../../kernel-pack -cfg ./ips2d.cfg -d 1 -p
 pfaces -CG -k amytiss.cpu@../../kernel-pack -cfg ./ips4d.cfg -d 1 -p
 
 
