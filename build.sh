28ef69985988917ad231527e5dd1d6f710c90c8f
Mahmoud Khaled <eng.mk@msn.com>
2020-12-29 15:15:34 +0100

    cmake works on windows

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ KERNEL_NAME=amytiss
 rm -rf kernel-pack/$KERNEL_NAME
 
 # building ...
-mkdir build
+mkdir -p build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=$BUILDTYPE
 cmake --build . --config $BUILDTYPE
