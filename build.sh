5e30a7b9ee8b63be8e132ddadc44fc7de5eca912
M. Khaled Mahmoud <kmahmoud@argo.ai>
2022-12-14 11:32:01 +0100

    minor fix

--- build.sh
+++ build.sh
@@ -3,9 +3,15 @@
 # CMake cofiguration
 BUILDTYPE=Release
 KERNEL_NAME=amytiss
+CLEAN_BUILD=false
 
 # remove old build binaries
-rm -rf kernel-pack/$KERNEL_NAME
+if [ $CLEAN_BUILD = true ]
+then 
+  echo "I am doing a clean build!"
+  rm -rf kernel-pack/$KERNEL_NAME.driver
+  rm -rf build
+fi
 
 # building ...
 mkdir -p build
@@ -13,4 +19,3 @@ cd build
 cmake .. -DCMAKE_BUILD_TYPE=$BUILDTYPE
 cmake --build . --config $BUILDTYPE
 cd ..
-rm -rf build
